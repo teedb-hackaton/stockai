2f5d9ac11792008a3b8358548ec6e3354b99d59e
DavidX206 <davidachara2@gmail.com>
2024-07-30 01:56:47 +0100

    pnpm change

--- app/(chat)/chat/[id]/page.tsx
+++ app/(chat)/chat/[id]/page.tsx
@@ -1,58 +1,58 @@
-// import { type Metadata } from 'next'
-// import { notFound, redirect } from 'next/navigation'
+import { type Metadata } from 'next'
+import { notFound, redirect } from 'next/navigation'
+import { getChat, getMissingKeys } from '@/app/actions'
+import { Chat } from '@/components/chat'
+import { AI } from '@/lib/chat/actions'
+import { Session } from '@/lib/types'
 
-// import { getChat, getMissingKeys } from '@/app/actions'
-// import { Chat } from '@/components/chat'
-// import { AI } from '@/lib/chat/actions'
-// import { Session } from '@/lib/types'
+export interface ChatPageProps {
+  params: {
+    id: string
+  }
+}
 
-// export interface ChatPageProps {
-//   params: {
-//     id: string
-//   }
-// }
+export async function generateMetadata({
+  params
+}: ChatPageProps): Promise<Metadata> {
+  const session = {} as Session
 
-// export async function generateMetadata({
-//   params
-// }: ChatPageProps): Promise<Metadata> {
-//   const session = await auth()
+  if (!session?.user) {
+    return {}
+  }
 
-//   if (!session?.user) {
-//     return {}
-//   }
+  const chat = await getChat(params.id, session.user.id)
+  return {
+    title: chat?.title.toString().slice(0, 50) ?? 'Chat'
+  }
+}
 
-//   const chat = await getChat(params.id, session.user.id)
-//   return {
-//     title: chat?.title.toString().slice(0, 50) ?? 'Chat'
-//   }
-// }
+export default async function ChatPage({ params }: ChatPageProps) {
+  const session = {} as Session
+  const missingKeys = await getMissingKeys()
 
-// export default async function ChatPage({ params }: ChatPageProps) {
-//   const missingKeys = await getMissingKeys()
+  if (!session?.user) {
+    redirect(`/login?next=/chat/${params.id}`)
+  }
 
-//   if (!session?.user) {
-//     redirect(`/login?next=/chat/${params.id}`)
-//   }
+  const userId = session.user.id as string
+  const chat = await getChat(params.id, userId)
 
-//   const userId = session.user.id as string
-//   const chat = await getChat(params.id, userId)
+  if (!chat) {
+    redirect('/')
+  }
 
-//   if (!chat) {
-//     redirect('/')
-//   }
+  if (chat?.userId !== session?.user?.id) {
+    notFound()
+  }
 
-//   if (chat?.userId !== session?.user?.id) {
-//     notFound()
-//   }
-
-//   return (
-//     <AI initialAIState={{ chatId: chat.id, messages: chat.messages }}>
-//       <Chat
-//         id={chat.id}
-//         session={session}
-//         initialMessages={chat.messages}
-//         missingKeys={missingKeys}
-//       />
-//     </AI>
-//   )
-// }
+  return (
+    <AI initialAIState={{ chatId: chat.id, messages: chat.messages }}>
+      <Chat
+        id={chat.id}
+        session={session}
+        initialMessages={chat.messages}
+        missingKeys={missingKeys}
+      />
+    </AI>
+  )
+}
